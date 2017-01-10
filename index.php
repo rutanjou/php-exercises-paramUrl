1200cd4ace1a95edcc20eac01343e7509505dd60
Ru Tan <jutan2012@gmail.com>
2017-01-10 14:48:31 +0100

    modification Exer1 avce function if (isset ($_GET['nom']))

--- index.php
+++ index.php
@@ -4,10 +4,22 @@
 	<meta charset="UTF-8">
 	<title>URL</title>
 </head>
-<body>
-  
-	<a href="index.php?nom=Nemare&prenom=Jean
-">Lien</a>
+<body> 
+<p><a href="index.php?nom
+">Nom :</a></p>
+<p><a href="index.php?prenom">Prenom : </a></p>
+<?php
+$nom = 'Nemare';
+$prenom = 'Jean';
+if(isset ($_GET['nom'])) {
+	# code...
+	echo $nom;
+}
+
+if(isset ($_GET['prenom'])){
+	echo $prenom;
+}
+?>
 
 </body>
 </html>
