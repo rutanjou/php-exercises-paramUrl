317f3b83b9e7219c67bda8b547745754b9be568a
Ru Tan <jutan2012@gmail.com>
2017-01-10 14:12:40 +0100

    Exer1 done avec <a>Lien</a>

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>URL</title>
+</head>
+<body>
+  
+	<a href="index.php?nom=Nemare&prenom=Jean
+">Lien</a>
+
+</body>
+</html>
