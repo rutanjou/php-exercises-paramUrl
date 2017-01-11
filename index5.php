32893faf16155e0a9f69fa487f655606b00444b0
Ru Tan <jutan2012@gmail.com>
2017-01-11 10:14:23 +0100

    Exer5 function (isset($_GET['semaine']))

---
+++ index5.php
@@ -0,0 +1,23 @@
+<?php 
+$semaine = 12;
+if (isset ($_GET['semaine'])) {
+	
+	echo "il yas : $semaine semaine ";
+}else{
+	echo "rien";
+}
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<a href="index5.php?semaine
+">semaine</a>
+
+</body>
+</html>
+
+
+
